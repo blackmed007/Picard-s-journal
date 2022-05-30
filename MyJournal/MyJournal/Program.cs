c9cb3409c1d579ad63dcb4355b045ecb5a3a5cf6
blackmed007 <92275188+blackmed007@users.noreply.github.com>
2022-05-30 15:38:20 +0200

    Add files via upload

---
+++ MyJournal/MyJournal/Program.cs
@@ -0,0 +1,60 @@
+﻿namespace Journal
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Jean-Luc Picard’s journal \n");//main page
+            while (true)
+            {
+                Console.WriteLine("please type  <start> to start writting or <stop> to To end writing : \n");
+                string read = Console.ReadLine();
+                if (read == "start")
+                {
+                    Journal();
+                }
+                else
+                if (read == "stop")
+                {
+                    Environment.Exit(0);
+                }
+
+            }
+
+
+            static void Journal()//Journal
+            {
+                Console.Clear();
+                
+                string file = $"{DateTime.Now}.txt";
+
+                using (StreamWriter Diary = File.Exists(file) ? File.AppendText(file) : new StreamWriter($"current-date.txt"))
+                {
+                    Diary.Flush();
+                    Diary.WriteLine($"Captain’s log \n Stardate: <{DateTime.Now}> ");
+
+
+                    while (true)
+                    {
+                        string input = Console.ReadLine();
+
+                        if (input == "stop")
+                        {
+                            Diary.Flush();
+
+                            Environment.Exit(0);
+                        }
+
+                        Diary.WriteLine(input);
+                        Diary.WriteLine("Jean-Luc Picard");
+
+
+                    }
+
+                }
+
+            }
+        }
+    }
+}
